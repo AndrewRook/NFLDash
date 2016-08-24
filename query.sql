f6c7e97654f4e4f39a700d2980891eb5ba1a5dff
Andrew Schechtman-Rook <rook166@gmail.com>
2016-08-23 22:53:56 -0400

    figured out arrays

--- query.sql
+++ query.sql
@@ -23,7 +23,36 @@ and play.description not like '(__:__)'
 and agg_play.passing_att = 0 and agg_play.rushing_att = 0 and agg_play.kicking_xpa = 0 and agg_play.kicking_fga = 0 and agg_play.defense_sk = 0 and play.penalty = 0 and agg_play.punting_tot = 0 and agg_play.kicking_tot = 0 and agg_play.passing_twopta = 0 and agg_play.rushing_twopta = 0 and play.description not like '%Penalty%' and agg_play.punting_blk = 0 and play.description not like '%sacked%' and play.description not like '%extra point is Aborted%'
 limit 10;
 
-/* Full production query: */
+/* Testing getting player ids */
+select pp.drive_id,
+(array_agg((play.time).phase))[1] as quarter, (array_agg((play.time).elapsed::int))[1] as seconds_elapsed,
+(array_agg((play.yardline).pos::int))[1] as yardline,
+(array_agg(play.yards_to_go))[1] as yards_to_go,
+array_agg(pp.player_id) as player_ids
+from play_player pp
+inner join play on pp.gsis_id = play.gsis_id and pp.drive_id = play.drive_id and pp.play_id = play.play_id
+group by (pp.gsis_id, pp.drive_id, pp.play_id) limit 10;
+
+/* Full production query. Contains:
+yardline (-50 to 50)
+time (quarter, second tuple, needs to be broken up)
+yards_to_go
+play_type (run, pass, etc. There are a very tiny fraction of plays classified as unknown, which can probably be filtered out of the dashboard at some point (but WPA would need to be computed first)
+description (the full text of the play)
+
+To be included:
+offensive team
+home team
+away team
+season year
+week
+home/away records?
+yards gained?
+sacks?
+fumbles/ints?
+first down conversions
+players involved
+*/
 select play.yardline, play.time, play.yards_to_go,
 (case when agg_play.passing_att = 1 or agg_play.defense_sk = 1 or agg_play.passing_twopta = 1
       or play.description like '%sacked%' then 'pass'
@@ -45,3 +74,12 @@ and play.description not like 'End of %'
 and play.description not like 'Timeout at%'
 and play.description not like '(_:__)'
 and play.description not like '(__:__)'
+
+
+SELECT n.nspname AS "schema", t.typname
+     , string_agg(e.enumlabel, '|' ORDER BY e.enumsortorder) AS enum_labels
+FROM   pg_catalog.pg_type t 
+JOIN   pg_catalog.pg_namespace n ON n.oid = t.typnamespace 
+JOIN   pg_catalog.pg_enum e ON t.oid = e.enumtypid  
+WHERE  t.typname = 'field_pos'
+GROUP  BY 1,2;
