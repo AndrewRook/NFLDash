b7784a9c0fd0795560c0bbff9d32454c5f52cb97
Andrew Schechtman-Rook <rook166@gmail.com>
2016-10-04 21:34:48 -0400

    seems to have fixed #30

--- static/js/utilities.js
+++ static/js/utilities.js
@@ -3,35 +3,6 @@
      Distributed under the terms of the GNU General Public License v3
 */
 
-function make_barchart_test (ndx, colname, div_id, width, height, bin_width)
-{
-    var dim = ndx.dimension(function(d){ return d[colname];});
-    var group = dim.group();
-    if (bin_width != 1)
-    {
-	group = dim.group(function(d) {return Math.floor(d/bin_width)*bin_width;});
-    }
-    var chart = dc.barChart(div_id);
-    var min_x = dim.bottom(1)[0][colname] - bin_width / 2.;
-    var max_x = dim.top(1)[0][colname] + bin_width / 2.;
-    var num_bins = (max_x - min_x) / bin_width;
-    chart
-	.dimension(dim)
-	.group(group)
-	.margins({top: 10, right: 40, bottom: 30, left: 40})
-	.centerBar(true)
-	.x(d3.scale.linear().domain([min_x, max_x]))
-	.elasticY(true)
-	.barPadding(0.1)
-	.width(width).height(height);
-    //console.log(max_x, min_x, bin_width, num_bins);
-    if (bin_width != 1)
-    {
-	chart.xUnits(function(){return num_bins;});
-    }
-    return chart;
-}
-
 function make_barchart (ndx, colname, div_id, width, height, bin_width, y_axis_style)
 {
     var dim = ndx.dimension(function(d){ return d[colname];});
@@ -160,7 +131,7 @@ function show_hide_general_reset()
     }
 }
 
-function make_team_season_result_selector(ndx, select_name, column_name, datatable, player_dim)
+function make_season_result_selector(ndx, select_name, column_name, datatable, player_dim)
 {
     
     var dim = ndx.dimension(function(d){ return d[column_name];});
@@ -182,6 +153,73 @@ function make_team_season_result_selector(ndx, select_name, column_name, datatab
 			  });
     return dim;
 }
+
+function make_team_selector(ndx, select_name, offense_select_name, home_column, away_column, datatable, player_dim)
+{
+    
+    var dim = ndx.dimension(function(d){ return [d[home_column], d[away_column]];});
+    $(select_name).change(function()
+			  {
+			      $(this).siblings("button").css("background", "rgb(255, 255, 255) linear-gradient(rgb(255, 255, 255) 0px, rgb(224, 224, 224) 100%) ").css("color", "black").css("text-shadow", "0 1px 0 #fff");
+			      $(offense_select_name).prop("disabled", false);
+			      show_hide_general_reset();
+			      dim.filterAll();
+			      var values = $(this).val();
+			      if (values.length > 0)
+			      {
+				  $(this).siblings("button").css("background", "#3182BD").css("color", "white").css("text-shadow", "0px 0px 0px #fff");
+				  dim.filter(function(d) {
+				      return values.indexOf(String(d[0])) != -1 || values.indexOf(String(d[1])) != -1;
+				  });
+			      }
+			      else
+			      {	  
+				  $(offense_select_name).prop("disabled", true);
+			      }
+			      dc.redrawAll();
+			      RefreshTable(datatable, player_dim);
+			      $(offense_select_name).trigger("change");
+			      $(offense_select_name).selectpicker("refresh");
+			      
+			  });
+    return dim;
+}
+
+function make_offense_selector(ndx, select_name, team_select_name, offense_column, defense_column, datatable, player_dim)
+{
+    var dim = ndx.dimension(function(d){ return [d[offense_column], d[defense_column]];});
+    $(select_name).change(function()
+			  {
+			      $(this).siblings("button").css("background", "rgb(255, 255, 255) linear-gradient(rgb(255, 255, 255) 0px, rgb(224, 224, 224) 100%) ").css("color", "black").css("text-shadow", "0 1px 0 #fff");
+			      show_hide_general_reset();
+			      dim.filterAll();
+			      var values = $(this).val();
+			      var selected_teams = $(team_select_name).val();
+			      if (values.length > 0)
+			      {
+				  $(this).siblings("button").css("background", "#3182BD").css("color", "white").css("text-shadow", "0px 0px 0px #fff");
+				  
+				  if (values.length > 0 && selected_teams.length > 0)
+				  {
+				      
+				      var value = values[0];
+				      dim.filter(function(d) {
+					  if ((value == "offense" && String(selected_teams).indexOf(d[0]) != -1) || (value == "defense" && String(selected_teams).indexOf(d[1]) != -1))
+					  {
+					      return true;
+					  }
+					  else
+					  {
+					      return false;
+					  }
+				      });
+				  }
+			      } 
+			      dc.redrawAll();
+			      RefreshTable(datatable, player_dim);
+			  });
+}
+
 function make_home_selector(ndx, select_name, home_column, offense_column, datatable, player_dim)
 {
     var dim = ndx.dimension(function(d){ return d[offense_column] == d[home_column];});
