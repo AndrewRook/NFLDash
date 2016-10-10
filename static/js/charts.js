c9070d5c7dead30e1f5b88979f1692057e469c65
Andrew Schechtman-Rook <rook166@gmail.com>
2016-10-10 16:09:04 -0400

    started loading charts

---
+++ static/js/charts.js
@@ -0,0 +1,52 @@
+var charts = {}
+
+charts.data_count = function(ndx, count_class) {
+    
+    var group = ndx.groupAll();
+    dc.dataCount(count_class)
+	.dimension(ndx)
+	.group(group);
+}
+
+charts.make_barchart = function(ndx, colname, div_id, width, height, bin_width, y_axis_style) {
+    var dim = ndx.dimension(function(d){ return d[colname];});
+    var group = dim.group();
+    if (bin_width != 1)
+    {
+	group = dim.group(function(d) {return Math.floor(d/bin_width)*bin_width;});
+    }
+    if (y_axis_style == "sqrt")
+    {
+	group.reduce(function(p, v) {return Math.sqrt(Math.round(p*p) + 1);}, function(p, v) {return Math.sqrt(Math.round(p*p) - 1);}, function(p, v) {return 0;});
+    }
+    var chart = dc.barChart(div_id);
+    var min_x = dim.bottom(1)[0][colname] - bin_width / 2.;
+    var max_x = dim.top(1)[0][colname] + bin_width / 2.;
+    var num_bins = (max_x - min_x) / bin_width;
+    var right_margin = 40;
+    var left_margin = 40;
+    if (width / right_margin < 5.)
+    {
+	right_margin = 0.10*width;
+	left_margin = 0.10*width;
+    }
+    chart
+	.dimension(dim)
+	.group(group)
+	.margins({top: 10, right: right_margin, bottom: 30, left: left_margin})
+	.centerBar(true)
+	.x(d3.scale.linear().domain([min_x, max_x]))
+	.elasticY(true)
+	.barPadding(0.1)
+	.width(width).height(height);
+    //console.log(max_x, min_x, bin_width, num_bins);
+    if (bin_width != 1)
+    {
+	chart.xUnits(function(){return num_bins;});
+    }
+    if (y_axis_style == "sqrt")
+    {
+	chart.yAxis().tickFormat(function(v) {return v*v;});
+    }
+    return chart;
+}
--- static/js/io.js
+++ static/js/io.js
@@ -7,25 +7,21 @@ var io = {
 	d3.csv(filename)
 	    .row(function(row)
 		 {
-		     return row;
+		     parsed_row = {
+			 "wp": +row.wp*100.,
+			 "wpa": +row.wpa*100.,
+		     };
+		     return parsed_row;
 		 })
 	    .on("progress", function (request)
 		{
 		    if(request.responseURL == progress_url)
 		    {
 			utilities.update_progress_bar(progress_bar_id, progress_increment, max_progress_fraction);
-			// var curr_width = $(progress_bar_id).width();
-			// var max_width = $(progress_bar_id).offsetParent().width();
-			// var new_width = curr_width + progress_increment * max_width;
-			// if (new_width < max_progress_fraction * max_width)
-			// {
-			//     $(progress_bar_id).width(new_width);
-			// }
 		    }
 		})
 	    .get(function(error, player_data)
 		 {
-		     //console.log(player_data[0]['season_year'], player_data.length, count)
 		     callback(null, player_data);
 		 });
     },
